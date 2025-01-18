b481fb813d8b7f4ce6960dca824445254f6da9fc
FatahRozaq <fatah.rozaq20@gmail.com>
2025-01-18 22:32:22 +0700

    Practice looping and conditional state

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const amount = 12;
 
 if (amount > 10) {
-    console.log('large number');
+    for (let index = 0; index < 10; index++) {
+        console.log('large number');
+    }   
 } else {
     console.log('small number');
 }
+
+switch (amount) {
+    case 10:
+        console.log('10 Hag');
+        break;
+    default:
+        console.log('Not 10 Hag');
+        break;
+}
+
+
 
 console.log("It's my first node app");
